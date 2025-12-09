54470b0e8b219c5f2d4eb9038af0282d75ba66b9
goodyn-cloud <goodynwinclawyer@gmail.com>
2025-12-09 23:40:06 +0900

    presale done

---
+++ server/media_server.js
@@ -0,0 +1,36 @@
+const NodeMediaServer = require('node-media-server'),
+    config = require('./config/default').rtmp_server,
+    User = require('./database/Schema').User,
+    helpers = require('./helpers/helpers');
+
+nms = new NodeMediaServer(config);
+
+nms.on('prePublish', async (id, StreamPath, args) => {
+    let stream_key = getStreamKeyFromStreamPath(StreamPath);
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+
+    // Check if mongoose is connected
+    const mongoose = require('mongoose');
+    if (mongoose.connection.readyState !== 1) {
+        console.warn('MongoDB not connected, allowing stream without authentication');
+        return;
+    }
+
+    User.findOne({stream_key: stream_key}, (err, user) => {
+        if (!err) {
+            if (!user) {
+                let session = nms.getSession(id);
+                session.reject();
+            } else {
+                helpers.generateStreamThumbnail(stream_key);
+            }
+        }
+    });
+});
+
+const getStreamKeyFromStreamPath = (path) => {
+    let parts = path.split('/');
+    return parts[parts.length - 1];
+};
+
+module.exports = nms;
